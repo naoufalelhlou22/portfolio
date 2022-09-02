77018e63db95144ac66278402267395fb25b1b24
naoufalelhlou22 <112399719+naoufalelhlou22@users.noreply.github.com>
2022-09-02 19:59:46 +0100

    Update notify.php

--- forms/notify.php
+++ forms/notify.php
@@ -1,40 +1,15 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'naoufal.elhlou@etu.uae.ac.ma';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+if (isset($POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = "Notify me request"
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject ="Notify me request";
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  $mailTo = "naoufal.elhlou@etu.uae.ac.ma";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
   
-  $contact->smtp = array(
-    'host' => 'smtp.elasticemail.com',
-    'username' => 'naoufal.elhlou@etu.uae.ac.ma',
-    'password' => '5133464756A02D1CC201CE16567FCB6AF5E7',
-    'port' => '2525'
-  );
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
   
-
-  $contact->add_message( $_POST['name'], 'Name');
-  $contact->add_message( $_POST['email'], 'Email');
-
-  echo $contact->send();
 ?>
